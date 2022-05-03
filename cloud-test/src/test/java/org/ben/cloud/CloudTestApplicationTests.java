15d26675477d9a4436c7a10b255d9ad6e8f63d06
Benoit Blancard <004328706@LAPTOP-FKPF4SH4>
2022-05-03 11:57:32 +0200

    Initialisation du projet de test

---
+++ cloud-test/src/test/java/org/ben/cloud/CloudTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ben.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
