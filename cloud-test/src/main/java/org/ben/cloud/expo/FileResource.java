6f7180c83abd425d2ab8e7c503fcda394aca3e47
Benoit Blancard <004328706@LAPTOP-FKPF4SH4>
2022-05-03 17:29:38 +0200

    Ajout de gestion d'un nom

--- cloud-test/src/main/java/org/ben/cloud/expo/FileResource.java
+++ cloud-test/src/main/java/org/ben/cloud/expo/FileResource.java
@@ -1,6 +1,8 @@
 package org.ben.cloud.expo;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,9 +12,13 @@ public class FileResource {
 		// TODO Auto-generated constructor stub
 	}
 	
-	@GetMapping("/hello")
-	public String getHello() {
-		return "Hello, world";
+	@GetMapping(value = {"/hello", "/hello/{name}"})
+	public String getHello(@PathVariable(name = "name", required = false) String name) {
+		if(StringUtils.isNotBlank(name)) {
+			return "Hello, " + name;
+		} else {
+			return "Hello, world";
+		}
 	}
 
 }
